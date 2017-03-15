98ab61c77795bea3b0c75ea8244c261d50704bc2
MPacheco21303 <caribe.pacheco@gmail.com>
2017-03-15 15:31:04 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,11 @@
+<?php
+echo "hola mundo php";
+?>
+<html>
+<head>
+<title>practica 10 php</title>
+</head>
+<body>
+<h1> hola mundo html</h1>
+</body>
+</html>
---
+++ practica10/index2.php
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+<title>practica 10 php</title>
+</head>
+<body>
+<h1> usando PHP</h1>
+     <p><?php
+	 echo "parrafo php";
+	 <h3>
+	 <?php
+	 ?></p>
+	 <p>parrafo HTML</p>
+	 ?>
+	 </h3>
+</body>
+</html>
